03f4626076a938911855de88908a95b579def1e3
Baek-HongYeol <bac25450@gmail.com>
2020-08-11 11:32:06 +0900

    use Firebase RealtimeDB instead of FTP library
    - FTP removed
    - TESTSSID removed
    - Firebase RTDB used
    - In connection with STASSID, send the status every 10 seconds.

--- entry_device.ino
+++ entry_device.ino
@@ -1,19 +1,21 @@
 /*
   version 1.1 - previous bool값 변경 지점 수정.
               - 부팅 직후를 분기하는 부분 삭제.
+              
+  version 2.0 - FTP 제거,
+              - TESTSSID 제거.
+              - Firebase RealTime Database 적용.
+              - 연결 상태에는 정보를 계속 업로드.
+  
 */
 #include <Arduino.h>
-#include <FTP_Client.h>
+#include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
 
 #ifndef STASSID
 #define STASSID "jesusschool"
 #define STAPSK  "****"
 #endif
-/*#ifndef TESTSSID
-#define TESTSSID  "****"
-#define TESTPSK   "****"
-#endif*/
 
 #define serialDBG
 
@@ -23,9 +25,8 @@ const char*     password = STAPSK;
 const char*     host = "192.168.0.35"; //192.168.0.1 ftp 서버 주소
 const uint16_t  port = 20;            //20 ftp 서버 포트 번호
 
-const char      carNumber[128] = "01";
+const String carNumber = "01";
 
-FTP_Client      ftp;
 
 int isJesusschool = 0;
 int isTestAP = 0;
@@ -56,6 +57,7 @@ void connectWiFi(const char* _ssid, const char* _passwd) {
   Serial.println(ESP.getFreeHeap());
   Serial.println("");
   #endif
+  Firebase.begin("mytestproject-905ac.firebaseio.com", "***************************");
 }
 
 void scanWiFiList() {
@@ -68,7 +70,9 @@ void scanWiFiList() {
   for (int i = 0; i < num_Of_Networks; i++) {
     if (WiFi.SSID(i).equals(STASSID)) {
       Serial.println("StaAP Founded--");
+      Serial.print("RSSI: ");
       jesusschool = WiFi.RSSI(i);
+      Serial.println(jesusschool);
       isJesusschool = 1;
       signal_IO = true;
     }
@@ -100,73 +104,83 @@ void scanWiFiList() {
       #endif
       connectWiFi(STASSID, STAPSK);
     }
-    #ifdef TESTSSID
-    else if (testAP>-100) {      //testAP != -100 || jesusschool<=testAP (if jesusschool==0, jesusschool>testAP)
-      #ifdef serialDBG
-      Serial.println();
-      Serial.println("connect TestAP...");
-      #endif
-      connectWiFi(TESTSSID, TESTPSK);
-    }
-    #endif
   }
 }
 
-bool ftp_sendStatus(bool sig){
-  if (ftp.openConnection()) {
-    delay(2000);
-    #ifdef serialDBG
-    Serial.println("----create File to server----");
-    #endif
-    if(ftp.initFile(FTP_FileType_ASCII)){
-      char filename[128];
-      snprintf(filename,128, "[%s]", carNumber);
-      if(!ftp.deleteFile(filename)){ //파일이 존재하지 않을 경우 오류발생, 연결 재시도
-        #ifdef serialDBG
-        Serial.println("--deleting Failed.--");
-        #endif
-        if(!ftp.openConnection()){ 
-          #ifdef serialDBG
-          Serial.println("----closing connection----");
-          #endif
-          ftp.closeConnection();
-          #ifdef serialDBG
-          Serial.println("--deleting Failed.--");
-          #endif
-          return false;
-        }
-        ftp.initFile(FTP_FileType_ASCII);
-      }
-      if(ftp.newFile(filename)) {
-        char message[128];
-        if(sig){
-          snprintf(message, 128, "IN\tRSSI: %d\nbooted %dms ago ", WiFi.RSSI(), millis());
-        } else{
-          sprintf(message, "OUT");
-        }
-        ftp.write(message);
-        #ifdef serialDBG
-        Serial.println("----data written to server----");
-        #endif
-        ftp.closeFile();
-        #ifdef serialDBG
-        Serial.println("----File closed----");
-        #endif
-        return true;
-      }
+String path = "EntryInfo/"+carNumber;
+bool sendIn(){
+  long atime = millis();
+  //IO 변경
+  Firebase.setBool(path+"/IO", true);
+  if (Firebase.failed()) {
+      Serial.print("updating /IO failed:");
+      Serial.println(Firebase.error());
       return false;
-    }
-    
-    // Close the connection
-    #ifdef serialDBG
-    Serial.println();
-    Serial.println("----closing connection----");
-    Serial.println();
-    #endif
-    ftp.closeConnection();
-    return true;
   }
-  return false;
+  Serial.println("update /IO");
+  //RSSI 변경
+  int rss = WiFi.RSSI();
+  Firebase.setInt(path+"/RSSI", rss);
+  if (Firebase.failed()) {
+      Serial.print("updating /RSSI failed:");
+      Serial.println(Firebase.error());
+      return false;
+  }
+  Serial.println("update /RSSI");
+  //bootedTime 변경
+  long bootedTime = millis();
+  String text = String(bootedTime);
+  Firebase.setString(path+"/bootedTime", text+"ms");
+  if (Firebase.failed()) {
+      Serial.print("updating /bootedTime failed:");
+      Serial.println(Firebase.error());
+      return false;
+  }
+  Serial.println("update /bootedTime");
+   Serial.print("function spends ");
+  Serial.print(millis()-atime);
+  Serial.println("ms.");
+  return true;
+}
+bool sendOut(){
+  long atime = millis();
+  // IO 변경
+  Firebase.setBool(path+"/IO", false);
+  if (Firebase.failed()) {
+      Serial.print("updating /IO failed:");
+      Serial.println(Firebase.error());
+      return false;
+  }
+  // RSSI 변경
+  int rss = WiFi.RSSI();
+  Firebase.setInt(path+"/RSSI", rss);
+  if (Firebase.failed()) {
+      Serial.print("updating /RSSI failed:");
+      Serial.println(Firebase.error());
+      return false;
+  }
+  // bootedTime 변경
+  long bootedTime = millis();
+  String text = String(bootedTime);
+  Firebase.setString(path+"/bootedTime", text+"ms");
+  if (Firebase.failed()) {
+      Serial.print("updating /bootedTime failed:");
+      Serial.println(Firebase.error());
+      return false;
+  }
+  Serial.print("function spends ");
+  Serial.print(millis()-atime);
+  Serial.println("ms.");
+  return true;
+}
+
+bool sendStatus(bool sig){
+  if(sig){
+    return sendIn();
+  }
+  else{
+    return sendOut();
+  }
 }
 
 bool isChanged(){
@@ -192,35 +206,25 @@ void setup() {
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
-  ftp.begin(host, port, "guest", "a", 15000);
-  connectWiFi(STASSID, STAPSK);
 }
-
+unsigned long startTime = 0;
 void loop() {
   scanWiFiList();
   //Serial.print("RSSI: ");
   //Serial.println(WiFi.RSSI());
-  //ftp_contentList();
+  //delay(500);
   unsigned long current=0;
-  
-  if(isChanged() && WiFi.status() == WL_CONNECTED){
-    //if(millis() < 100000){
-      for(current = millis(); millis() - current < 20000;){
-        if(ftp_sendStatus(signal_IO)){
-          previous = signal_IO;
-          break;
-        }
-        delay(1000);
+
+  //when IO state changed, or every 10 seconds when it have In state, sendStatus.
+  if((isChanged() || (millis()-startTime >10000 && signal_IO))&& WiFi.status() == WL_CONNECTED){
+    startTime = millis();
+    for(current = startTime; millis() - current < 10000;){
+      if(sendStatus(signal_IO)){
+        previous = signal_IO;
+        break;
       }
-    /*}else{
-      for(current = millis(); millis() - current < 20000;){
-        if(ftp_sendStatus(signal_IO)){
-          previous = signal_IO
-          break;
-        }
-        delay(1000);
-      }
-    }*/
+      delay(500);
+    }
   }
   
   #ifdef serialDBG
